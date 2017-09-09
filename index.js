9f8300445cd868901076070c36b00844e648bbe2
Axel Dolce <axel.dolce@gmail.com>
2017-09-09 16:22:04 -0300

    refactor

--- index.js
+++ index.js
@@ -7,58 +7,8 @@ const SELECT = {
 var { Jurisdiction, Job, Person } = sequelize.models
 var { createPerson, createPersonJob } = require('./services/people')
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync().then(() => {
     return Promise.all([])
-}).then(() => {
-
-    return Promise.all([
-        Jurisdiction.create({
-            name: 'Argentina'
-        }).then(arg => {
-            return Promise.all([
-                createPerson({ name: 'Mauricio Macri', ssn: '20131204699' }),
-                createPerson({ name: 'Patricia Bullrich', ssn: '27119883364' }),
-                createPerson({ name: 'Pablo Nocetti', ssn: '20181103745' }),
-                Job.create({ name: 'Presidente de la Nación', JurisdictionId: arg.id }),
-                Job.create({ name: 'Ministro de Seguridad de la Nación', JurisdictionId: arg.id }),
-                Job.create({ name: 'Jefe de Gabinete de Ministerio de Seguridad de la Nación', JurisdictionId: arg.id }),
-                Jurisdiction.create({
-                    name: 'Buenos Aires', parentJurisdictionId: arg.id
-                }).then(bsas => {
-                    return Promise.all([
-                        Jurisdiction.create({
-                            name: 'Vicente López',
-                            parentJurisdictionId: bsas.id
-                        }),
-                        Jurisdiction.create({
-                            name: 'San Isidro',
-                            parentJurisdictionId: bsas.id
-                        }),
-                        Jurisdiction.create({
-                            name: 'Capital Federal',
-                            parentJurisdictionId: bsas.id
-                        })
-                    ])
-                }),
-                Jurisdiction.create({
-                    name: 'Chubut', parentJurisdictionId: arg.id
-                }).then(chubut => {
-                    return Promise.all([
-                        Jurisdiction.create({
-                            name: 'Esquel',
-                            parentJurisdictionId: chubut.id
-                        })
-                    ])
-                })
-            ])
-        }).then(() => {
-            var p = findPersonIdByLastName('Bullrich')
-            return Promise.all([
-                createPersonJob(p, 1, '2015-12-10')
-            ])
-        })
-    ])
-
 }).then(() => {
 
 
@@ -84,9 +34,3 @@ sequelize.sync({ force: true }).then(() => {
 
 var is2o = is => is.map(i2o)
 var i2o = i => i.get()
-
-function findPersonIdByLastName(lname) {
-    return Person.findOne({
-        where: { lastName: 'Bullrich' }
-    }).then(p => p ? p.id : p)
-}
