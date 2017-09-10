da06f62c137df79076a0398ca23e3f820e681b68
Axel Dolce <axel.dolce@gmail.com>
2017-09-10 20:15:02 -0300

    updte

--- scripts/populate.js
+++ scripts/populate.js
@@ -1,7 +1,8 @@
 var sequelize = require('../sequelize')
+sequelize.options.logging = null
 
 var { Jurisdiction, Job, Person, PersonJob } = sequelize.models
-var { createPerson, createPersonJob, findPersonIdByLastName } = require('../services/people')
+var { createPerson, createPersonJob, findPersonIdByLastName, findPersonIdBySSN } = require('../services/people')
 
 function createARG() {
     return Jurisdiction.findOrCreate({
@@ -21,10 +22,16 @@ Promise.all([]).then(() => {
                 createPerson({ name: 'Mauricio Macri', ssn: '20131204699' }).catch(err => null),
                 createPerson({ name: 'Patricia Bullrich', ssn: '27119883364' }).catch(err => null),
                 createPerson({ name: 'Pablo Nocetti', ssn: '20181103745' }).catch(err => null),
+                createPerson({ name: 'Elisa Carrió', ssn: '27135920326' }).catch(err => null),
+                createPerson({ name: 'Cristina Fernández de Kirchner', ssn: '27104336154' }).catch(err => null),
                 Job.create({ name: 'Presidente de la Nación', JurisdictionId: arg.id }).catch(err => null),
                 Job.create({ name: 'Ministro de Seguridad de la Nación', JurisdictionId: arg.id }).catch(err => null),
                 Job.create({ name: 'Jefe de Gabinete de Ministerio de Seguridad de la Nación', JurisdictionId: arg.id }).catch(err => null),
-                Job.create({ name: 'Diputada de la Nación por la Ciudad de Buenos Aires', JurisdictionId: arg.id }).catch(err => null),
+                Job.create({ name: 'Senador de la Nación por la Ciudad de Buenos Aires', JurisdictionId: arg.id }).catch(err => null),
+                Job.create({ name: 'Diputado de la Nación por la Ciudad de Buenos Aires', JurisdictionId: arg.id }).catch(err => null),
+                Job.create({ name: 'Senador de la Nación por la provincia de Buenos Aires', JurisdictionId: arg.id }).catch(err => null),
+                Job.create({ name: 'Senador de la Nación por la provincia de Santa Cruz', JurisdictionId: arg.id }).catch(err => null),
+                Job.create({ name: 'Diputado de la Nación por la provincia de Santa Cruz', JurisdictionId: arg.id }).catch(err => null),
                 Jurisdiction.findOrCreate({
                     where: {
                         name: 'Buenos Aires', parentJurisdictionId: arg.id
@@ -39,10 +46,16 @@ Promise.all([]).then(() => {
                             name: 'San Isidro',
                             parentJurisdictionId: bsas.id
                         }).catch(err => null),
-                        Jurisdiction.create({
-                            name: 'Capital Federal',
-                            parentJurisdictionId: bsas.id
-                        }).catch(err => null)
+                        Jurisdiction.findOrCreate({
+                            where: {
+                                name: 'Capital Federal',
+                                parentJurisdictionId: bsas.id
+                            }
+                        }).then(([caba]) => {
+                            return Promise.all([
+                                Job.create({ name: 'Jefe de Gobierno de la Ciudad Autónoma de Buenos Aires', JurisdictionId: caba.id }).catch(err => null)
+                            ])
+                        })
                     ])
                 }),
                 Jurisdiction.findOrCreate({
@@ -58,13 +71,6 @@ Promise.all([]).then(() => {
                     ])
                 })
             ])
-        }).then(() => {
-            var p = findPersonIdByLastName('Bullrich')
-            return Promise.all([
-                createPersonJob(p, 1, '2015-12-10'),
-                createPersonJob(p, 3, '2007-12-10', '2015-12-10'),
-                createPersonJob(p, 3, '1993-12-10', '1997-12-10')
-            ]).catch(err => console.log(err))
         })
     ])
 }).catch(err => {
@@ -74,6 +80,7 @@ Promise.all([]).then(() => {
         //Jurisdiction.all().then(js => console.log(js.map(j => j.get()))),
         Jurisdiction.count().then(c => console.log('Jur', c)),
         Person.count().then(c => console.log('People', c)),
-        PersonJob.count().then(c => console.log('PeopleJobs', c))
+        PersonJob.count().then(c => console.log('PeopleJobs', c)),
+        Job.count().then(c => console.log('Jobs', c))
     ])
 })
