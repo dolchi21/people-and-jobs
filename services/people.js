df7aeedc916be3e26fd39349eeb91b5b1d226d5e
Axel Dolce <axel.dolce@gmail.com>
2017-09-10 00:33:30 -0300

    find by ssn

--- services/people.js
+++ services/people.js
@@ -1,23 +1,24 @@
 var sequelize = require('../sequelize')
+var knex = require('knex')({
+    client: 'sqlite'
+})
 
 var Person = sequelize.model('Person')
 
 function createPerson({ name = required('name'), ssn }) {
     var [firstName, lastName] = name.split(' ')
     return Person.create({
-        firstName, lastName
-    }).catch(err => {
-        console.error('Error')
+        ssn, firstName, lastName
     })
 }
 
-function createPersonJob(PersonId = required('PersonId'), JobId = required('JobId'), start = required(), end = required()) {
+function createPersonJob(PersonId = required('PersonId'), JobId = required('JobId'), start = required(), end) {
     var PersonJob = sequelize.model('PersonJob')
     return Promise.all([PersonId, JobId, start, end]).then(([PersonId, JobId, start, end]) => {
         return PersonJob.create({
             PersonId: PersonId, JobId: JobId,
             start, end
-        }).catch(err => console.error(err))
+        })
     })
 }
 function findPersonIdByLastName(lname) {
@@ -25,15 +26,34 @@ function findPersonIdByLastName(lname) {
         where: { lastName: lname }
     }).then(p => p ? p.id : p)
 }
+function findBySSN(ssn = required('SSN')) {
+    return Person.findOne({
+        where: { ssn },
+        include: [{ all: true }]
+    }).then(p => {
+        return {
+            ssn: p.ssn,
+            name: [p.firstName, p.lastName].join(' '),
+            jobs: p.PersonJobs.map(pj => pj.get())
+        }
+    })
+}
 
-Object.assign(module.exports, {
-    createPerson,
-    createPersonJob,
-    findPersonIdByLastName
-})
+function personJobs(personId) {
+    return sequelize.model('PersonJob').all({
+        where:{
+            
+        }
+    })
+}
 
 function required(data) {
     var err = new Error('RequiredError')
     err.data = data
     throw err
 }
+
+exports.createPerson = createPerson
+exports.createPersonJob = createPersonJob
+exports.findPersonIdByLastName = findPersonIdByLastName
+exports.findBySSN = findBySSN
