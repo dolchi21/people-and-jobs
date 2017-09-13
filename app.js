712772c5f3adee7fcf3aa310fd873b719639ff12
Axel Dolce <axel.dolce@gmail.com>
2017-09-13 18:08:33 -0300

    npm install morgan body-parser

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+
 var app = express()
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
 app.use('/api', require('./routes/api'))
 
 module.exports = app
