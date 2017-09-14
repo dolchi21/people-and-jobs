febc54c948592195622f278401ecff11d9695536
Axel Dolce <axel.dolce@gmail.com>
2017-09-14 19:37:53 -0300

    simulate internet

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ var app = express()
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
+app.use('/api', (req, res, next) => {
+    setTimeout(next, 1000)
+})
 app.use('/api', require('./routes/api'))
+
+var appGetRoot = {
+    start: new Date()
+}
+app.get('/', (req, res, next) => {
+    res.json({
+        data: appGetRoot
+    })
+})
 
 module.exports = app
