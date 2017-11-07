be3ed97b2e4520544f83744baa0c2c8e6f558687
Axel Dolce <axel.dolce@gmail.com>
2017-11-07 20:58:24 -0300

    cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express')
+var cors = require('cors')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
 
 var app = express()
+
+app.use(cors())
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
